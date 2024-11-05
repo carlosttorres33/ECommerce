4242aa5c1c8523e093e982132d8cc0538be70d0f
Carlos Torres <kisamehatake33@comunidad.unam.mx>
2024-11-05 14:22:01 -0600

    Login Form Init Design

--- app/src/main/java/com/carlostorres/ecommerce/MainActivity.kt
+++ app/src/main/java/com/carlostorres/ecommerce/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,9 +15,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,7 +43,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ECommerceTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    
+                    LoginContent()
                 }
             }
         }
@@ -50,9 +56,9 @@ fun LoginContent() {
 
     Box(
         modifier = Modifier
-        .fillMaxSize()
-    ){
-        
+            .fillMaxSize()
+    ) {
+
         Image(
             modifier = Modifier
                 .fillMaxSize(),
@@ -88,8 +94,79 @@ fun LoginContent() {
                 fontSize = 20.sp
             )
 
+            Spacer(modifier = Modifier.weight(1f))
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(
+                    topStart = 40.dp,
+                    topEnd = 40.dp,
+                    bottomStart = 0.dp,
+                    bottomEnd = 0.dp
+                ),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White
+                )
+            ) {
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    Text(
+                        modifier = Modifier
+                            .padding(bottom = 20.dp),
+                        text = "Ingresar",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        color = Color.Black
+                    )
+
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        value = "",
+                        onValueChange = {},
+                        label = {
+                            Text(text = "Correo Electrónico")
+                        }
+                    )
+
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        value = "",
+                        onValueChange = {},
+                        label = {
+                            Text(text = "Contraseña")
+                        }
+                    )
+
+                    Button(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        onClick = {  }
+                    ) {
+                        Text(text = "Iniciar Sesion")
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .padding(top = 10.dp)
+                    ) {
+                        Text(text = "No tienes cuenta?")
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Text(text = "Registrate", color = Color.Blue)
+                    }
+
+                }
+
+            }
+
         }
-
     }
-
 }
